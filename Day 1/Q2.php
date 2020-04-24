9d660c7b77828239fc26210420ad483590807499
Rishen Valayodapillai <rishen.t.pillai@gmail.com>
2020-04-24 15:44:16 +0400

    all elements are printed with their keys

--- Day 1/Q2.php
+++ Day 1/Q2.php
@@ -3,4 +3,10 @@ $arrayChar = Array();
 for ($i = 65; $i < 91; $i++) { 
     $arrayChar[] = chr($i); 
 }
+// Loop to display the array elements 
+foreach ($arrayChar as $key => $value) { 
+    // Display the key and its 
+    // value of an array  
+    echo $key . " => " . $value . ","; 
+} 
 ?>
