6dd8fbb5cdf624d331b1a4965b8e6fe79bdad356
Rishen Valayodapillai <rishen.t.pillai@gmail.com>
2020-04-26 18:30:38 +0400

    produce first value in number plate

--- Day 1/Q3.php
+++ Day 1/Q3.php
@@ -2,3 +2,12 @@
 $numberPlate = array("",""," ","0","0","1"," ","A","A"," ");
 
 
+function prodFirstLetter($numberPlate)
+{
+    for ($i = 0; $i <= 10; $i++) {
+        for ($i = 65; $i <= 90; $i++) {
+            $numberPlate[0] = chr($i);
+            prodSecondLetter($numberPlate);
+        };
+    };
+}
